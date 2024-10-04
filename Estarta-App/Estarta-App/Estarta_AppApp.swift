8915389d478f4724b5e014e5b925cb2edee8d460
Oday Moahmmed <odaydieg@gmail.com>
2024-10-04 20:50:44 +0300

    Create new Xcode Project 🎉

---
+++ Estarta-App/Estarta-App/Estarta_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estarta_AppApp.swift
+//  Estarta-App
+//
+//  Created by eng-oday on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Estarta_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
